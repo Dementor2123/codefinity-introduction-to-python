187a30e8e72151dfe0bddc26e360c593b35d1e62
Dementor2123 <>
2026-01-30 19:21:30 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,10 @@
+ #Calculate the total cost of 5 
+
+item_name = "Soda"
+item_price = 6.99
+purchase_quantity = 5
+total_cost = item_price * purchase_quantity
+print("total_cost_for", purchase_quantity, "cases_of", item_name, "is $", total_cost )
+
+
+
