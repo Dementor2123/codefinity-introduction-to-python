8f407fa967a7ada318c554870ec6c714c572485a
Dementor2123 <>
2026-01-30 14:56:45 +0000

    Variable Assignment

--- variables_and_types/variable_assignment/main.py
+++ variables_and_types/variable_assignment/main.py
@@ -1,6 +1,27 @@
 item_name = "banana"
-print("Item : ", item_name)
+print("Item: ", item_name)
 
 # Reassigning the variable
 item_name = "Eggs"
-print("Item",item_name)
+print("Item:",item_name)
+
+item_price = 3.80
+print("Price: $", item_price)
+
+# Reassigning the variable
+item_price = 3.99
+print("Price: $", item_price)
+
+item_stock_quantity = 100
+print("Quantity:", item_stock_quantity)
+
+# Reassigning the variable
+item_stock_quantity = 120
+print("Quantity:", item_stock_quantity)
+
+in_stock = 400
+print("In Stock:", in_stock)
+
+# Reassigning the variable
+in_stock = True
+print("In stock:", in_stock)
