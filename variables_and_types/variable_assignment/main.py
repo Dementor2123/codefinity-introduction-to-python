a2260dd9fb7cdc7d21ecf20d0aafd8fdaad0022e
Dementor2123 <>
2026-01-30 14:37:28 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,9 @@
+item_name = "Eggs"
+item_price = 3.99
+item_stoke_quantity = 120
+in_stoke = True
+# Reassigning the variable
+print("Item : ", Eggs)
+print("Price per unit: $", 3.99)
+print("quantity in stock: ", 120)
+print("In stock: ", True)
